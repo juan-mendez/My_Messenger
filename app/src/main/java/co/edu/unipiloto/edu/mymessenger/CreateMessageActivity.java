18175c8e24359966648e49beaacc09ccdbf84691
juan-mendez <juan-mendez7@upc.edu.co>
2021-01-15 01:04:38 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/edu/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,32 @@
+package co.edu.unipiloto.edu.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //se llama cuando click en el Button
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        //Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hello");
+
+        startActivity(intent);
+
+    }
+}
